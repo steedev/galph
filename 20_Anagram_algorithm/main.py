acc420a245a7c58bf013ef41cd45ee0904d05a29
stee <steedev0@gmail.com>
2021-05-15 15:41:31 +0100

    Add correct anagram code

--- 20_Anagram_algorithm/main.py
+++ 20_Anagram_algorithm/main.py
@@ -1,16 +1,17 @@
 def anagram(w1, n1, w2, n2):
     if n1 != n2:
         return False
-    q1 = 0
+    A = [0] * 127
     for i in range(n1):
-        q1 += ord(w1[i])
-        q1 -= ord(w2[i])
-    if q1 != 0:
-        return False
+        A[ord(w1[i])] += 1
+        A[ord(w2[i])] -= 1
+    for i in range(127):
+        if A[i] != 0:
+            return False
     return True
 
 
-w1 = 'rabk'
+w1 = 'krab'
 w2 = 'bark'
 print(anagram(w1, len(w1), w2, len(w2)))
 
